6ab251d3fbcea464271c73dd38b9c4c66bdfce37
Julijanas Jezov <julijanas.jezov@gmail.com>
2015-11-22 13:04:09 +0000

    merge after success

---
+++ merge.sh
@@ -0,0 +1,11 @@
+GH_TOKEN = 038a1052519222dc31b94323137ed6d562a5fa9d
+if [ "$TRAVIS_BRANCH" != "test" ]; then     
+	exit 0;
+Fi
+
+git config -- global user.email= “julijanas.jezov@gmail.com”
+git config -- global user.name = “julijanas jezov”
+
+git checkout master || exit
+git merge "$TRAVIS_COMMIT" || exit
+git push origin master $GH_TOKEN@github.com/JulijanasJezov/im-client.git

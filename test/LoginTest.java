bcf6db933b9556ef4abc8a501e3ac6d3bdbdda31
Julijanas Jezov <julijanas.jezov@gmail.com>
2015-11-23 15:00:57 +0000

    first UI test

--- test/LoginTest.java
+++ test/LoginTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.edt.GuiQuery;
+import org.assertj.swing.fixture.FrameFixture;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -7,9 +11,35 @@ import org.junit.Test;
 
 public class LoginTest {
 	
-	@Test
-	public void test_fake() {
-		assertTrue(true);
-	}
+	private FrameFixture window;
+
+	  @BeforeClass
+	  public static void initialise() {
+	    FailOnThreadViolationRepaintManager.install();
+	  }
+
+	  @Before
+	  public void openWindow() {
+	    LoginSwing frame = GuiActionRunner.execute(new GuiQuery<LoginSwing>() {
+	      protected LoginSwing executeInEDT() {
+	        return new LoginSwing();
+	      }
+	    });
+	    
+	    window = new FrameFixture(frame);
+	    window.show(); // shows the frame to test
+	  }
+
+	  @Test
+	  public void test_initialWindow() {
+		  String result = window.textBox("txtFieldUsername").text();
+		  
+		  assertTrue("checking the username textbox is empty", result.equals(""));
+	  }
+
+	  @After
+	  public void closeWindow() {
+	    window.cleanUp();
+	  }
 	
 }

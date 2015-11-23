8ca5ca5d066760ee2be7177894e4904fe049b024
Julijanas Jezov <julijanas.jezov@gmail.com>
2015-11-23 17:13:49 +0000

    add initial login window ui tests

--- test/LoginTestUI.java
+++ test/LoginTestUI.java
@@ -1,8 +1,12 @@
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+
+import static org.assertj.swing.core.MouseButton.LEFT_BUTTON;
 import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.edt.GuiQuery;
+import org.assertj.swing.fixture.ColorFixture;
 import org.assertj.swing.fixture.FrameFixture;
 import org.junit.After;
 import org.junit.Before;
@@ -27,16 +31,16 @@ public class LoginTestUI {
 	    });
 	    
 	    window = new FrameFixture(frame);
-	    window.show(); // shows the frame to test
 	  }
 
 	  @Test
 	  public void test_initialWindow() {
 		  String result = window.textBox("txtFieldUsername").text();
-		  
 		  assertTrue("checking the username textbox is empty", result.equals(""));
+		  assertEquals("checking the login button text", "Login", window.button("btnLogin").text());
+		  assertEquals("checking the username label text", "Username", window.label("lblUsername").text());
 	  }
-
+	  
 	  @After
 	  public void closeWindow() {
 	    window.cleanUp();

1ebb2f50161383f1b1c6389a64804b172fdb3255
Julijanas Jezov <julijanas.jezov@gmail.com>
2015-11-24 11:55:30 +0000

    refactor first tests

--- test/ChatClientTest.java
+++ test/ChatClientTest.java
@@ -14,22 +14,13 @@ import org.junit.Test;
 public class ChatClientTest {
 
 	private static ChatClient chatClient;
-    private Server server;
+    private static Server server;
 	
 	@BeforeClass
-	public static void init() {
+	public static void init() throws InterruptedException {
 		chatClient = ChatClient.getInstance();
-	}
-	
-	@Test
-	public void test_notConnected() {
 		assertFalse("checking if the connection is established", chatClient.isServerConnectionEstablished());
-		assertFalse("checking if the user logged in", chatClient.isLoggedIn());
-		assertTrue("checking the current user", chatClient.getCurrentUser() == null);
-	}
-
-	@Test
-	public void test_loggedIn() throws IOException, InterruptedException {
+		
 		Thread serverRun = new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -38,10 +29,24 @@ public class ChatClientTest {
 	    }, "manage users");
 		serverRun.start();
 		
-		Thread.sleep(1000);
-		
+		Thread.sleep(1000); // Allow some time for server to start
+
 		chatClient.connectToServer();
-		
+	}
+	
+	@Test
+	public void test_connected() {
+		assertTrue("check if connected to the server", chatClient.isServerConnectionEstablished());
+	}
+	
+	@Test
+	public void test_notLoggedIn() {
+		assertFalse("checking if the user logged in", chatClient.isLoggedIn());
+		assertTrue("checking the current user", chatClient.getCurrentUser() == null);
+	}
+
+	@Test
+	public void test_loggedIn() throws IOException, InterruptedException {
 		chatClient.login("testUser");
 		
 		assertTrue("checking if the user logged in", chatClient.isLoggedIn());
